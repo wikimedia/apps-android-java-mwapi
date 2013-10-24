e903c32deb9642c10a90a563220d7aab27a9c157
YuviPanda <yuvipanda@gmail.com>
2013-10-25 01:24:54 +0530

    Add tests proving that POST works
    Also checking to make sure that GETs are GETs
    
    Change-Id: I310857a3b6d4d5c5ccd93ff5238ed030bfc81d8a

--- src/test/java/org/mediawiki/api/ApiTests.java
+++ src/test/java/org/mediawiki/api/ApiTests.java
@@ -11,7 +11,32 @@ import org.junit.*;
 public class ApiTests {
 
     @Test
-    public void testBasicParse() throws Exception {
+    public void testBasicPost() {
+        Api api = new Api("test.wikipedia.org");
+        // We're just checking if the POST goes through, so does not
+        // matter which username password we use
+        String testUsername = "doesntmatter";
+        String testPassword = "doesntreallymattertome";
+
+        JSONObject resp = api.action("login")
+                .param("lgname", testUsername)
+                .param("lgpassword", testPassword)
+                .post();
+        assertEquals(resp.optJSONObject("login").optString("result"), "NeedToken");
+        assertNull(resp.optJSONObject("error"));
+    }
+
+    @Test
+    public void testWrongMethod() {
+        Api api = new Api("test.wikipedia.org");
+        JSONObject resp = api.action("login")
+                .get();
+
+        assertEquals(resp.optJSONObject("error").optString("code"), "mustbeposted");
+    }
+
+    @Test
+    public void testBasicGet() throws Exception {
         Api api = new Api("test.wikipedia.org");
         String inputText = "Test String";
         String inputTitle = "Test Title";
